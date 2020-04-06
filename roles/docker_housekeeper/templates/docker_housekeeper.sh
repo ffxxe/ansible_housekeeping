80dc182b6cd2e5028533adeb5f94a8cb9f8d6728
ffxxe <49100733+ffxxe@users.noreply.github.com>
2020-04-06 18:11:38 +0300

    Fix readability log

--- roles/docker_housekeeper/templates/docker_housekeeper.sh
+++ roles/docker_housekeeper/templates/docker_housekeeper.sh
@@ -10,23 +10,27 @@ exec 1>>/var/log/docker_housekeeper.log
 exec 2>>/var/log/docker_housekeeper.err
 
 {% raw %}
-docker ps -a --format 'table {{.ID}}\t{{.Status}}' |grep Exited |grep months |awk '{print $1}' |xargs --no-run-if-empty docker rm
 echo "$(date) : Remove all stopped containers older than a few months" >&1
+#Output container info to log
+docker ps -a --format 'table {{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.Status}}' |grep Exited |grep months
+#Remove container
+docker ps -a --format 'table {{.ID}}\t{{.Status}}' |grep Exited |grep months |awk '{print $1}' |xargs --no-run-if-empty docker rm &>/dev/null
 echo "---" >&1
 {% endraw %}
 
-docker image prune -a --force --filter "until={{ item.value.housekeeper_hours }}h"
 echo "$(date) : Remove all unused images older than {{ item.value.housekeeper_hours }} hours" >&1
+docker image prune -a --force --filter "until={{ item.value.housekeeper_hours }}h"
 echo "---" >&1
 
-docker network prune --force --filter "until={{ item.value.housekeeper_hours }}h"
 echo "$(date) : Remove all unused networks older than {{ item.value.housekeeper_hours }} hours" >&1
+docker network prune --force --filter "until={{ item.value.housekeeper_hours }}h"
 echo "---" >&1
 
-docker builder prune --force --filter "until={{ item.value.housekeeper_hours }}h"
 echo "$(date) : Remove build cache older than {{ item.value.housekeeper_hours }} hours" >&1
+docker builder prune --force --filter "until={{ item.value.housekeeper_hours }}h"
 echo "---" >&1
 
-#docker volume prune --force
 #echo "$(date) : Remove all unused local volumes (not supported "until")" >&1
+#docker volume prune --force
 #echo "---" >&1
+exit 0
